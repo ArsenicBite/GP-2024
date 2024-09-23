1503d62ed54fc76236f06137659540ba8b3f79e8
Arsenic <81708177+ArsenicBite@users.noreply.github.com>
2024-09-23 16:40:11 +0100

    Moving Watermelon
    Water Melon Can Move

--- the-course/sprite_2d.gd
+++ the-course/sprite_2d.gd
@@ -2,12 +2,25 @@ extends Sprite2D
 #Extends an existing class
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:  #_ready is whenever the node is ready
+func _ready():  #_ready is whenever the node is ready
 	pass # Replace with function body.
 
-var speed = 2
+@export var speed:float
+@export var rot_speed:float
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	translate(Vector2(3, speed * delta))
+func _process(delta: float):
+	#rotate(deg_to_rad(rot_speed * delta))
+	#translate(Vector2(3, speed * delta))
+	#global_translate(Vector2(0, speed * delta))
+	if Input.is_key_pressed(KEY_UP):
+		translate(Vector2(0, - speed * delta))
+	if Input.is_key_pressed(KEY_DOWN):
+		translate(Vector2(0, + speed * delta))
+	if Input.is_key_pressed(KEY_LEFT):
+		translate(Vector2(- speed * delta, + 0))
+		rotate(deg_to_rad(-rot_speed * delta))
+	if Input.is_key_pressed(KEY_RIGHT):
+		translate(Vector2(speed * delta, + 0))
+		rotate(deg_to_rad(rot_speed * delta))
 	pass
