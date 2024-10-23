f3a8b9ffb48a4d5f8dbccdfb0fdeb12cb059e8df
Arsenic <81708177+ArsenicBite@users.noreply.github.com>
2024-10-23 17:32:49 +0100

    Begun Adding Notes to the piano

--- music-assaignment/main_game.gd
+++ music-assaignment/main_game.gd
@@ -1,1 +1,19 @@
 extends Node2D
+#1
+func _on_1_pressed() -> void:
+	$"(1)".play()
+#2
+func _on_2_pressed() -> void:
+	$"(2)".play()
+#3
+func _on_3_pressed() -> void:
+	$"(3)".play()
+#4
+func _on_4_pressed() -> void:
+	$"(4)".play()
+
+func _on_5_pressed() -> void:
+	$"(5)".play()
+
+func _on_6_pressed() -> void:
+	$"(6)".play()
